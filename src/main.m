fbe4dd8f8afcc1f005041944c17e5caf3cfcde20
SoraTrails <me876657621@126.com>
2017-01-16 11:36:38 +0800

    build a hmm in main.m

---
+++ src/main.m
@@ -0,0 +1,78 @@
+%main
+%generate observe array
+%states : 
+%		  mid
+%		  front
+%		  front_feature
+%		  feature
+%		  
+%observe: main_color 2
+%		  main_area 3 = 6
+%
+%1 : true,small
+%2 : true,mid
+%3 : true,large
+%4 : false,small
+%5 : false,mid
+%6 : false,large
+%
+size = 7000;
+fid=fopen('main_c','r');  
+temp = [];
+seq = zeros(size/5,1);
+j=1;
+for i=1:size
+    text = fgetl(fid);
+	if mod(i,5) ~= 0
+		continue;
+	end
+    a=sscanf(text,'frame:%d ratio:%lf h:%d s:%d v:%d');%frame:1 ratio:0.304685 h:0 s:1 v:3
+	% temp = [temp,a];
+	c=2;
+    d=3;
+    if a(2) < 0.2164
+        c=1;
+    elseif a(2) > 0.2861
+        c=3;
+    end
+    if a(3) == 0 && a(4) == 1 && a(5) == 3
+        d=0;
+    end
+    seq(j) = c + d; 
+	j = j+1;
+end
+
+ob = zeros(100,1);
+for i=1:100
+	text = fgetl(fid);
+    a=sscanf(text,'frame:%d ratio:%lf h:%d s:%d v:%d');%frame:1 ratio:0.304685 h:0 s:1 v:3
+	c=2;
+    d=3;
+    if a(2) < 0.2164
+        c=1;
+    elseif a(2) > 0.2861
+        c=3;
+    end
+    if a(3) == 0 && a(4) == 1 && a(5) == 3
+        d=0;
+    end
+    ob(i) = c + d; 
+end
+fclose(fid);  
+
+TRGUESS = [0.8,0.1,0.05,0.05;...
+		   0.05,0.8,0.1,0.05;...
+		   0.05,0.05,0.8,0.1;...
+		   0.1,0.05,0.05,0.8];
+
+EMITGUESS = [0.02,0.18,0.8,0,0,0;...
+			 0.1,0.8,0.1,0,0,0;...
+			 0.8,0.18,0.02,0,0,0;...
+			 0,0,0,0.33,0.33,0.34];
+
+[ESTTR,ESTEMIT] = hmmtrain(seq,TRGUESS,EMITGUESS);
+						   % 'Statenames',{'mid';'front';'front_feature';'feature'});
+
+state = hmmviterbi(ob,ESTTR,ESTEMIT,...
+				   'Statenames',{'mid';'front';'front_feature';'feature'});
+
--- src/test.m
+++ src/test.m
@@ -1,82 +1,58 @@
-intra = zeros(2);
-intra(1,2) = 1; % node 1 in slice t connects to node 2 in slice t
+% intra = zeros(2);
+% intra(1,2) = 1; % node 1 in slice t connects to node 2 in slice t
 
-inter = zeros(2);
-inter(1,1) = 1; % node 1 in slice t-1 connects to node 1 in slice t
+% inter = zeros(2);
+% inter(1,1) = 1; % node 1 in slice t-1 connects to node 1 in slice t
 
-% We can specify the parameters as follows, where for simplicity we assume the observed node is discrete.
+% % We can specify the parameters as follows, where for simplicity we assume the observed node is discrete.
 
-Q = 2; % num hidden states
-O = 2; % num observable symbols
+% Q = 2; % num hidden states
+% O = 2; % num observable symbols
 
-ns = [Q O];
-dnodes = 1:2;
-bnet = mk_dbn(intra, inter, ns, 'discrete', dnodes);
-for i=1:4
-  bnet.CPD{i} = tabular_CPD(bnet, i);
-end
-
-
-% file = fopen('edge_data','w');
-
-% for frame=1:NumberOfFrames
-	% ima=read(video,frame);%原图象
-	% ima_gray = rgb2gray(ima);
-
-	% [ima_edge, threshold] = edge(ima_gray,'canny',[0.2,0.6]);
-	% [H, theta, rho] = hough(ima_edge);
-	% P = houghpeaks(H,5,'threshold',ceil(0.2*max(H(:))));
-	% lines = houghlines(ima_edge,theta,rho,P,'FillGap',8,'MinLength',20);
-
-	% %link
-	% if isempty(lines)
-		% fprintf(file, '%d:null\n',frame);
-		% continue;
-	% end
-
-	% % merge
-	% new_lines = [];
-
-	% for i=1:length(lines)
-		% if lines(i).theta == -90
-			% continue;
-		% end
-		% flag = 0;
-		% for j=1:length(new_lines)
-			% temp_k = tand(lines(i).theta + 90);
-			% temp_b1 = lines(i).point1(2) - temp_k * lines(i).point1(1);
-			% temp_b2 = new_lines(j).point1(2) - temp_k * new_lines(j).point1(1);
-			% if abs(temp_b2 - temp_b1) < 3 %able to link
-				% if lines(i).theta ~= 0
-					% if lines(i).point1(1) < new_lines(j).point1(1)
-						% new_lines(j).point1 = lines(i).point1;
-					% end
-					% if lines(i).point2(1) > new_lines(j).point2(1)
-						% new_lines(j).point2 = lines(i).point2;
-					% end
-				% else
-					% if lines(i).point1(2) < new_lines(j).point1(2)
-						% new_lines(j).point1 = lines(i).point1;
-					% end
-					% if lines(i).point2(2) > new_lines(j).point2(2)
-						% new_lines(j).point2 = lines(i).point2;
-					% end
-				% end
-				% flag = 1;
-				% break;
-			% end
-		% end
-		% if flag == 0 %not able to link
-			% new_lines = [new_lines, lines(i)];
-		% end
-		% i = i + 1;
-	% end
-
-	% fprintf(file, '%d:',frame);
-	% for s=1:length(new_lines)
-		% fprintf(file, '%d    ',new_lines(s).theta);
-	% end
-	% fprintf(file,'\n');
-
+% ns = [Q O];
+% dnodes = 1:2;
+% bnet = mk_dbn(intra, inter, ns, 'discrete', dnodes);
+% for i=1:4
+  % bnet.CPD{i} = tabular_CPD(bnet, i);
 % end
-% fclose(file);
+
+%%%%%%%%%%%%%%%%%%%%%%%%HMM
+% trans=[0,1;1,0];
+% emis =[1/6,1/6,1/6,1/6,1/6,1/6;
+       % 1/10,1/10,1/10,1/10,1/10,1/2];
+% % [seq,states] = hmmgenerate(100,trans,emis)
+% [seq,states] = hmmgenerate(100,trans,emis,...
+    % 'Symbols',{'one','two','three','four','five','six'},...
+    % 'Statenames',{'fair';'loaded'});
+% % [estimateTR,estimateE] = hmmestimate(seq,states,...
+% %     'Symbols',{'one','two','three','four','five','six'},...
+% %     'Statenames',{'fair';'loaded'});
+% count = 0;
+% for i=1:100
+   % if strcmp(seq{i},'two')
+       % count = count + 1;
+   % end
+% end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+size = 7573;
+fid=fopen('main_c','r');  
+
+seq = zeros(size,1);
+for i=1:size
+    text = fgetl(fid);
+    a=sscanf(text,'frame:%d ratio:%f h:%d s:%d v:%d');%frame:1 ratio:0.304685 h:0 s:1 v:3
+	
+    seq(i) = a(2); 
+end
+fclose(fid);
+
+seq = sort(seq);
+
+up = seq(fix(size/3));
+
+down = seq(fix(size*2/3));
+
+
+
+
